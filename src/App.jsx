f296babaff82be156d04ed379964a7435cd93c82
Girija-Kangutkar <kangutkargirija075@gmail.com>
2025-06-20 01:03:46 +0530

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,14 @@ import "./App.css";
 import Medicines from "./pages/Medicines";
 import Navbar from "./components/NavBar";
 
-
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <AllRoutes />
-    </div>
-  );
-=======
     <>
-
-      <Navbar/>
+      <Navbar />
       <h1>App</h1>
-      <Medicines/>
+      <Medicines />
     </>
   );
-
->>>>>>> 5562ce7b815d99bc0e1db8f6a9c60986ebfdd055
 };
 
 export default App;
