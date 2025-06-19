75f5d5ce8639ff44b8f292f3b0fbc68b286b318f
Raghav Singh <dhankarraghav13@gmail.com>
2025-06-19 23:37:56 +0530

    Navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "../components/nav.css"; 
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+  
+    <nav class='h-18 flex justify-center gap-5 py-4 bg-gray-100 shadow-md'>
+      <div className = 'navBar' >
+        
+        <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">Home</button>
+        <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">Medicine</button>
+        <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">Contact</button>
+        <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">About Us</button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
