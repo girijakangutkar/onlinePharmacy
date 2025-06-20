b219a2230230418bb5d2e08386d601093c7b5d80
Raghav Singh <dhankarraghav13@gmail.com>
2025-06-21 00:02:17 +0530

    isActive added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,8 @@ import About from "../pages/About";
 import Home from "../pages/Home";
 // import Contact from "../pages/Contact";
 
+
+
 const NavBar = () => {
   return (
     <nav className="h-18 flex justify-center gap-5 py-4 bg-gray-100 shadow-md">
@@ -14,6 +16,15 @@ const NavBar = () => {
         <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">Medicine</button>
         <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">Contact</button>
         <button class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300">About Us</button> */}
+        {/* <NavLink
+          to="/messages"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+          class="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300"
+        >
+          Messages
+        </NavLink> */}
         <NavLink className="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300" to="/">Home</NavLink>
         <NavLink className="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300" to="/about">About Us</NavLink>
         <NavLink className="px-4 py-4 hover:bg-green-500 hover:text-white  rounded transition duration-300" to="/contact">Contact Us</NavLink>
